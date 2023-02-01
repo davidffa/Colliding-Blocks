19ec435fe6b063a26b8c7022e2c34c8859699791
David Amorim <davidfilipeamorim@gmail.com>
2023-02-01 02:29:47 +0000

    Fix build script

--- build.sh
+++ build.sh
@@ -6,5 +6,7 @@ CFLAGS="-Wall -Wextra"
 INCLUDES=`pkg-config --cflags sdl2`
 LIBS=`pkg-config --libs sdl2`
 
-mkdir build
+if [ ! -d build ]; then
+    mkdir build
+fi
 cc $CFLAGS $INCLUDES -o ./build/colliding_blocks main.c $LIBS
